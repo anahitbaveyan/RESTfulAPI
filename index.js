9d3f176948364905145759d939fed55e8d334f5a
anahitbaveyan <baveyanan03@gmail.com>
2023-05-25 21:18:31 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+app.use(express.json());
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);  
+});
